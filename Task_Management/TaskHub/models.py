da06dc49849bf56b16b9c2ed9fcadafd0ebf8a46
Arya Deshmukh <aryaad2003@gmail.com>
2023-01-13 20:32:20 +0530

    updated model

--- Task_Management/TaskHub/models.py
+++ Task_Management/TaskHub/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+class User(models.Model):
+    firstname = models.CharField(max_length=150)
+    lastname = models.CharField(max_length=150)
+    email = models.CharField(max_length=150)
+    contact = models.IntegerField(max_length=9999999999)
+    password = models.CharField(max_length=150)
 
 # Create your models here.
 class Groups(models.Model):
