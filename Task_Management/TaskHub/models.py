b73b34b013ccefecc4c2709fbc8fc86996447271
shinjan9903 <shinjanchaturvedi7@gmail.com>
2023-01-13 20:29:33 +0530

    added models

--- Task_Management/TaskHub/models.py
+++ Task_Management/TaskHub/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class Groups(models.Model):
+    groupname=models.CharField(max_length=300)
+    owner=models.CharField(max_length=300)
+    groupdescrition=models.CharField(max_length=300)
+    members=models.ManyToManyField(User)
+
+
+class Tasks(models.Model):
+    taskname=models.CharField(max_length=300)
+    completionstatus=models.CharField(max_length=300)
+    assignedto=models.CharField(max_length=300)
+    taskdescription=models.CharField(max_length=300)
+    groupname=models.ForeignKey(Groups, on_delete=models.CASCADE)
